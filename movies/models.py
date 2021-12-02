c4db0815bcbe10e3a02e815b6aad382c033bd490
Dima Lol <zonex1995@gmail.com>
2021-12-02 19:29:35 +0200

    Added basic view/main page

--- movies/models.py
+++ movies/models.py
@@ -57,7 +57,7 @@ class Movie(models.Model):
     directors = models.ManyToManyField(Actor, verbose_name="режиссер", related_name="film_director")
     actors = models.ManyToManyField(Actor, verbose_name="актеры", related_name="film_actor")
     genres = models.ManyToManyField(Genre, verbose_name="жанры")
-    world_premiere = models.DateTimeField("Примьера в мире", default=datetime.date.today)
+    world_premiere = models.DateField("Примьера в мире", default=datetime.date.today)
     budget = models.PositiveIntegerField("Бюджет", default=0, help_text="указывать сумму в долларах")
     fees_in_usa = models.PositiveIntegerField("Сборы в США", default=0, help_text="указывать сумму в долларах")
     fees_in_world = models.PositiveIntegerField("Сборы в мире", default=0, help_text="указывать сумму в долларах")
